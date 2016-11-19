00f82f810cdd3c7b2f8b323691f13ea035990da2
robs <rob.swartenbroekx@cegeka.com>
2016-11-19 20:27:14 +0100

    small refactoring

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -2,8 +2,7 @@ import { App } from '../../src/app';
 
 describe('the app', () => {
   it('should have an array with failed builds retrieved from the buildservice by initialization', (done) => {
-    let failedBuildsPromise = Promise.resolve(['a', 'b', 'c']);
-    let buildServiceStub = { getAllFailedBuilds: (done) => failedBuildsPromise }
+    let buildServiceStub = { getAllFailedBuilds: (done) => Promise.resolve(['a', 'b', 'c']) }
     let app = new App(buildServiceStub);
     expectAtEndOfEventLoop(() => expect(app.builds).toEqual(['a', 'b', 'c']))
 

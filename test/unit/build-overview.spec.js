a996f64675155cabbce5c45711e240e8e52a3875
robs <rob.swartenbroekx@cegeka.com>
2016-11-20 18:12:18 +0100

    cleanup test

--- test/unit/build-overview.spec.js
+++ test/unit/build-overview.spec.js
@@ -3,20 +3,17 @@ import { BuildOverview } from '../../src/build-overview';
 describe('the build overview', () => {
   it('activate function should ask the failedbuilds from buildservice with the baseurl from the parameters and set the result on the build overview', (done) => {
     let buildServiceStub = {
-      getAllFailedBuilds: function getAllFailedBuilds (baseUrl) {
+      getAllFailedBuilds: function getAllFailedBuilds(baseUrl) {
         expect(baseUrl).toEqual("baseUrl");
         return Promise.resolve(['a', 'b', 'c'])
       }
     }
     let buildOverview = new BuildOverview(buildServiceStub);
-    buildOverview.activate({baseUrl: "baseUrl"});
-    expectAtEndOfEventLoop(() => expect(buildOverview.builds).toEqual(['a', 'b', 'c']))
+    buildOverview.activate({ baseUrl: "baseUrl" });
+    expectAtEndOfJobQueue(() => expect(buildOverview.builds).toEqual(['a', 'b', 'c']))
 
-    function expectAtEndOfEventLoop(expectFunction) {
-      setTimeout(() => {
-        expectFunction();
-        done();
-      }, 0);
+    function expectAtEndOfJobQueue(expectFunction) {
+      Promise.resolve().then(expectFunction).finally(done);
     }
   });
 });

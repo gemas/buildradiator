81c4accfe46344f3cce00d6b1f3b844c43fbccb7
robisrob <rob.swartenbroekx@gmail.com>
2016-12-03 10:09:11 +0100

    Add files via upload
    Trying to publish radiator

---
+++ radiator/scripts/app-bundle.js
@@ -0,0 +1,341 @@
+define('app',['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var App = exports.App = function () {
+    function App() {
+      _classCallCheck(this, App);
+    }
+
+    App.prototype.configureRouter = function configureRouter(config, router) {
+      this.router = router;
+      config.title = 'Teamcity radiator';
+      config.map([{ route: ':baseUrl', name: 'Build Overview', moduleId: 'build-overview' }]);
+    };
+
+    return App;
+  }();
+});
+define('build-overview',['exports', 'services/build-service', 'aurelia-framework'], function (exports, _buildService, _aureliaFramework) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.BuildOverview = undefined;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var _dec, _class;
+
+  function setAllFailedBuilds(params) {
+    var _this = this;
+
+    this.service.getAllFailedBuilds(params.baseUrl).then(function (builds) {
+      _this.builds = builds;
+    });
+  }
+
+  var BuildOverview = exports.BuildOverview = (_dec = (0, _aureliaFramework.inject)(_buildService.BuildService), _dec(_class = function () {
+    function BuildOverview(service) {
+      _classCallCheck(this, BuildOverview);
+
+      this.service = service;
+    }
+
+    BuildOverview.prototype.activate = function activate(params) {
+      setAllFailedBuilds.bind(this)(params);
+      setInterval(setAllFailedBuilds.bind(this), 30000, params);
+    };
+
+    return BuildOverview;
+  }()) || _class);
+});
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+
+  var _environment2 = _interopRequireDefault(_environment);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  Promise.config({
+    warnings: {
+      wForgottenReturn: false
+    }
+  });
+
+  function configure(aurelia) {
+    aurelia.use.standardConfiguration().feature('resources');
+
+    if (_environment2.default.debug) {
+      aurelia.use.developmentLogging();
+    }
+
+    if (_environment2.default.testing) {
+      aurelia.use.plugin('aurelia-testing');
+    }
+
+    aurelia.start().then(function () {
+      return aurelia.setRoot();
+    });
+  }
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('services/build-service',['exports', './http-client-router', 'aurelia-framework'], function (exports, _httpClientRouter, _aureliaFramework) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.BuildService = undefined;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var _dec, _class;
+
+  var BuildService = exports.BuildService = (_dec = (0, _aureliaFramework.inject)(_httpClientRouter.HttpClientRouter), _dec(_class = function () {
+    function BuildService(clientRouter) {
+      _classCallCheck(this, BuildService);
+
+      this.clientRouter = clientRouter;
+    }
+
+    BuildService.prototype.getAllFailedBuilds = function getAllFailedBuilds(baseUrl) {
+      var url = 'http://' + baseUrl + '/guestAuth/app/rest/buildTypes?locator=affectedProject:(id:_Root)&fields=buildType(id,name,builds($locator(running:false,canceled:false,count:1),build(number,status,statusText)))';
+
+      var init = {
+        method: 'GET',
+        headers: new Headers({
+          'Accept': 'application/json',
+          'X-Requested-With': 'Fetch'
+        })
+      };
+
+      return this.clientRouter.fetch(url, init).then(function (response) {
+        return response.json();
+      }).then(function (jsonResponse) {
+        return jsonResponse.buildType.filter(function (buildTypeElement) {
+          return buildTypeElement.builds.build.length > 0;
+        }).map(function (buildTypeElement) {
+          return {
+            "name": buildTypeElement.name,
+            "status": buildTypeElement.builds.build[0].status,
+            "statusText": buildTypeElement.builds.build[0].statusText
+          };
+        }).filter(function (build) {
+          return build.status === 'FAILURE';
+        });
+      });
+    };
+
+    return BuildService;
+  }()) || _class);
+});
+define('services/http-client-router',['exports', 'aurelia-fetch-client', './teamcitystub/team-city-http-client-stub', 'aurelia-framework'], function (exports, _aureliaFetchClient, _teamCityHttpClientStub, _aureliaFramework) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    exports.HttpClientRouter = undefined;
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var _dec, _class;
+
+    var HttpClientRouter = exports.HttpClientRouter = (_dec = (0, _aureliaFramework.inject)(_aureliaFetchClient.HttpClient, _teamCityHttpClientStub.TeamCityHttpClientStub), _dec(_class = function () {
+        function HttpClientRouter(realHttpClient, teamCityHttpClientStub) {
+            _classCallCheck(this, HttpClientRouter);
+
+            this.realHttpClient = realHttpClient;
+            this.teamCityHttpClientStub = teamCityHttpClientStub;
+        }
+
+        HttpClientRouter.prototype.fetch = function fetch(url, init) {
+            return url.includes('stub') ? this.teamCityHttpClientStub.fetch() : this.realHttpClient.fetch(url, init);
+        };
+
+        return HttpClientRouter;
+    }()) || _class);
+});
+define('services/teamcitystub/team-city-builds-response',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    "buildType": [{
+      "name": "build 1",
+      "builds": {
+        "build": [{
+          "status": "SUCCESS",
+          "statusText": "Tests passed: 198, ignored: 9"
+        }]
+      }
+    }, {
+      "name": "build 2",
+      "builds": {
+        "build": [{
+          "status": "SUCCESS",
+          "statusText": "Tests passed: 2391, ignored: 6"
+        }]
+      }
+    }, {
+      "name": "build 3",
+      "builds": {
+        "build": [{
+          "status": "SUCCESS",
+          "statusText": "Tests passed: 35"
+        }]
+      }
+    }, {
+      "name": "build 4",
+      "builds": {
+        "build": [{
+          "status": "SUCCESS",
+          "statusText": "Tests passed: 35"
+        }]
+      }
+    }, {
+      "name": "build 5",
+      "builds": {
+        "build": [{
+          "status": "FAILURE",
+          "statusText": "Tests failed: 4 (1 new), passed: 31"
+        }]
+      }
+    }, {
+      "name": "build 6",
+      "builds": {
+        "build": [{
+          "status": "FAILURE",
+          "statusText": "Tests failed: 2, passed: 33; snapshot dependency failed: Main :: Data Quality Tests :: build 6"
+        }]
+      }
+    }, {
+      "name": "build 7",
+      "builds": {
+        "build": [{
+          "status": "FAILURE",
+          "statusText": "Tests failed: 3, passed: 32; snapshot dependency failed: Main :: Data Quality Tests :: build 7"
+        }]
+      }
+    }, {
+      "name": "build 8",
+      "builds": {
+        "build": [{
+          "status": "FAILURE",
+          "statusText": "Tests failed: 8 (1 new), passed: 27; snapshot dependency failed: Main :: Data Quality Tests :: build 8"
+        }]
+      }
+    }, {
+      "name": "build 9",
+      "builds": {
+        "build": [{
+          "status": "FAILURE",
+          "statusText": "Tests failed: 13, passed: 22; snapshot dependency failed: Main :: Data Quality Tests :: build 9"
+        }]
+      }
+    }, {
+      "name": "build 10",
+      "builds": {
+        "build": [{
+          "status": "FAILURE",
+          "statusText": "Tests failed: 10, passed: 25"
+        }]
+      }
+    }]
+  };
+});
+define('services/teamcitystub/team-city-http-client-stub',['exports', './team-city-builds-response'], function (exports, _teamCityBuildsResponse) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.TeamCityHttpClientStub = undefined;
+
+  var _teamCityBuildsResponse2 = _interopRequireDefault(_teamCityBuildsResponse);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var TeamCityHttpClientStub = exports.TeamCityHttpClientStub = function () {
+    function TeamCityHttpClientStub() {
+      _classCallCheck(this, TeamCityHttpClientStub);
+    }
+
+    TeamCityHttpClientStub.prototype.fetch = function fetch() {
+      return Promise.resolve({ json: function json() {
+          return _teamCityBuildsResponse2.default;
+        } });
+    };
+
+    return TeamCityHttpClientStub;
+  }();
+
+  ;
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template>\r\n  <require from=\"css/custom.css\"></require>\r\n  <router-view></router-view>\r\n</template>"; });
+define('text!css/custom.css', ['module'], function(module) { module.exports = "\r\n\r\n"; });
+define('text!build-overview.html', ['module'], function(module) { module.exports = "<template>\r\n\t<div class=\"container\">\r\n\t\t<div class=\"row\">\r\n\t\t\t<div class=\"col-md-4 text-center\" repeat.for=\"build of builds\">\r\n\t\t\t\t<div class=\"alert alert-danger\" role=\"alert\">\r\n\t\t\t\t\t<h1>${build.name}</h1>\r\n\t\t\t\t\t<p>${build.statusText}</p>\r\n\t\t\t\t</div>\r\n\t\t\t</div>\r\n\t\t</div>\r\n\t</div>\r\n</template>"; });
+//# sourceMappingURL=app-bundle.js.map

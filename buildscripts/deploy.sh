21bb80b189733d59059240d88388ec9d6bab7293
Rob Swartenbroekx <rob.swartenbroekx@gmail.com>
2016-12-28 23:26:54 +0100

    trying to autodeploy

--- buildscripts/deploy.sh
+++ buildscripts/deploy.sh
@@ -2,4 +2,18 @@
 
 echo "New deploy\n"
 au build --env prod
+
+cd $HOME
 pwd
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "travis-ci"
+git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/robisrob/buildradiator gh-pages
+
+cd gh-pages/app
+cp $HOME/build/robisrob/buildradiator/index.html ./index.html
+cp $HOME/build/robisrob/buildradiator/scripts/app-bundle.js ./scripts/app-bundle.js
+cp $HOME/build/robisrob/buildradiator/scripts/vendor-bundle.js ./scripts/vendor-bundle.js
+
+git add -A
+git commit -m "new deploy: $TRAVIS_BUILD_NUMBER"
+git push 

7cf70ac8206f181ab96f32feb20a222cb99be5d2
Rob Swartenbroekx <rob.swartenbroekx@gmail.com>
2016-12-29 12:04:19 +0100

    trying to fix autodeploy

--- buildscripts/deploy.sh
+++ buildscripts/deploy.sh
@@ -13,6 +13,8 @@ mkdir -p gh-pages/app/scripts
 cp $HOME/build/robisrob/buildradiator/index.html ./gh-pages/app/index.html
 cp $HOME/build/robisrob/buildradiator/scripts/app-bundle.js ./gh-pages/app/scripts/app-bundle.js
 cp $HOME/build/robisrob/buildradiator/scripts/vendor-bundle.js ./gh-pages/app/scripts/vendor-bundle.js
+
+cd gh-pages
 git add -A
 git commit -m "new deploy: $TRAVIS_BUILD_NUMBER"
-git push 
+git push > /dev/null

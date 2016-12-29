ba4022e2c61618b6a7263b9f29f15032838eab04
Rob Swartenbroekx <rob.swartenbroekx@gmail.com>
2016-12-29 11:57:41 +0100

    improving deploy.sh

--- buildscripts/deploy.sh
+++ buildscripts/deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 echo "New deploy\n"
+
 au build --env prod
 
 cd $HOME
-pwd
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
 git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/robisrob/buildradiator gh-pages
 
 mkdir -p gh-pages/app/scripts
-cd gh-pages/app
-cp $HOME/build/robisrob/buildradiator/index.html ./index.html
-cp $HOME/build/robisrob/buildradiator/scripts/app-bundle.js ./scripts/app-bundle.js
-cp $HOME/build/robisrob/buildradiator/scripts/vendor-bundle.js ./scripts/vendor-bundle.js
-echo ${GH_TOKEN}
+cp $HOME/build/robisrob/buildradiator/index.html ./gh-pages/app/index.html
+cp $HOME/build/robisrob/buildradiator/scripts/app-bundle.js ./gh-pages/app/scripts/app-bundle.js
+cp $HOME/build/robisrob/buildradiator/scripts/vendor-bundle.js ./gh-pages/app/scripts/vendor-bundle.js
 git add -A
 git commit -m "new deploy: $TRAVIS_BUILD_NUMBER"
 git push 

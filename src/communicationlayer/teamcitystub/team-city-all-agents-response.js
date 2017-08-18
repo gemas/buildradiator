d4f0e16f71095ea01ddd9e23f16f9fbec02b38f6
robs <rob.swartenbroekx@cegeka.com>
2017-08-18 08:42:27 +0200

    Add agent stub responses

---
+++ src/communicationlayer/teamcitystub/team-city-all-agents-response.js
@@ -0,0 +1,84 @@
+export default {
+    "count": 13,
+    "href": "/guestAuth/app/rest/agents",
+    "agent": [
+        {
+            "id": 3,
+            "name": "SomeAgent_1",
+            "typeId": 3,
+            "href": "/guestAuth/app/rest/agents/id:3"
+        },
+        {
+            "id": 2,
+            "name": "SomeAgent_2",
+            "typeId": 2,
+            "href": "/guestAuth/app/rest/agents/id:2"
+        },
+        {
+            "id": 1,
+            "name": "SomeAgent_3",
+            "typeId": 1,
+            "href": "/guestAuth/app/rest/agents/id:1"
+        },
+        {
+            "id": 20,
+            "name": "SomeAgent_4",
+            "typeId": 20,
+            "href": "/guestAuth/app/rest/agents/id:20"
+        },
+        {
+            "id": 21,
+            "name": "SomeAgent_5",
+            "typeId": 21,
+            "href": "/guestAuth/app/rest/agents/id:21"
+        },
+        {
+            "id": 17,
+            "name": "SomeAgent_6",
+            "typeId": 17,
+            "href": "/guestAuth/app/rest/agents/id:17"
+        },
+        {
+            "id": 18,
+            "name": "SomeAgent_7",
+            "typeId": 18,
+            "href": "/guestAuth/app/rest/agents/id:18"
+        },
+        {
+            "id": 19,
+            "name": "SomeAgent_8",
+            "typeId": 19,
+            "href": "/guestAuth/app/rest/agents/id:19"
+        },
+        {
+            "id": 10,
+            "name": "SomeAgent_9",
+            "typeId": 10,
+            "href": "/guestAuth/app/rest/agents/id:10"
+        },
+        {
+            "id": 6,
+            "name": "SomeAgent_10",
+            "typeId": 6,
+            "href": "/guestAuth/app/rest/agents/id:6"
+        },
+        {
+            "id": 9,
+            "name": "SomeAgent_11",
+            "typeId": 9,
+            "href": "/guestAuth/app/rest/agents/id:9"
+        },
+        {
+            "id": 13,
+            "name": "SomeAgent_12",
+            "typeId": 13,
+            "href": "/guestAuth/app/rest/agents/id:13"
+        },
+        {
+            "id": 12,
+            "name": "SomeAgent_13",
+            "typeId": 12,
+            "href": "/guestAuth/app/rest/agents/id:12"
+        }
+    ]
+};
---
+++ src/communicationlayer/teamcitystub/team-city-notconnected-agents-response.js
@@ -0,0 +1,18 @@
+export default {
+    "count": 13,
+    "href": "/guestAuth/app/rest/agents?locator=connected:true",
+    "agent": [
+        {
+            "id": 9,
+            "name": "SomeAgent_11",
+            "typeId": 9,
+            "href": "/guestAuth/app/rest/agents/id:9"
+        },
+        {
+            "id": 19,
+            "name": "SomeAgent_8",
+            "typeId": 19,
+            "href": "/guestAuth/app/rest/agents/id:19"
+        },
+    ]
+};
